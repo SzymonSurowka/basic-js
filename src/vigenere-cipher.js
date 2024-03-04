e320eb811cd9fa06d13f2c63f36ea319e2e5bda2
SzymonSurowka <szymon121122@gmail.com>
2024-03-04 22:02:15 +0100

    feat: task update

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    return this.code(str, key, "encrypt");
+  }
+
+  decrypt(str, key) {
+    return this.code(str, key, "decrypt");
+  }
+
+  code(str, key, type) {
+    if (!str || !key || !type) throw Error("Incorrect arguments!")
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    const arr = [];
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if (str[i].match(/[A-Z]/)) {
+        if (type === "decrypt") {
+          arr.push(String.fromCharCode((str[i].charCodeAt(0) - "A".charCodeAt(0) + (26 - (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0)))) % 26 + "A".charCodeAt(0)));
+        } else {
+          arr.push(String.fromCharCode((str[i].charCodeAt(0) - "A".charCodeAt(0) + (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0))) % 26 + "A".charCodeAt(0)));
+        }
+        j++
+      } else {
+        arr.push(str[i]);
+      }
+    }
+    return this.direct ? arr.join("") : arr.reverse().join("");
   }
 }
 
