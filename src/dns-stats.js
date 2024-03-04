9cc59d88bd4e9b9170ccc9a54db9bf56d4bd3535
SzymonSurowka <szymon121122@gmail.com>
2024-03-04 20:27:51 +0100

    feat: task update

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+  domains.forEach(domain => {
+    const subdomains = domain.split('.').reverse();
+    let currentSubdomain = '';
+    for (let subdomain of subdomains) {
+      currentSubdomain += `.${subdomain}`;
+      dnsStats[currentSubdomain] = (dnsStats[currentSubdomain] || 0) + 1;
+    }
+  });
+  return dnsStats;
 }
 
 module.exports = {
